961aba4bec418dc8dac4485bad547aee663c66ad
koulight <koulight@hotmail.com>
2022-07-31 17:19:16 -0700

    Homepage loads correctly

--- server.js
+++ server.js
@@ -24,54 +24,54 @@ app.use(express.json());
 
 // ROUTES
 app.get('/', async (request, response) => {
-  const boughtItems = await db.collection('jungle').find().toArray(); 
-  const cartCount = await db 
-    .collection('jungle') 
-    .countDocuments(); 
-  response.render('index.ejs', { items: todoItems, count: cartCount }); 
+//   const boughtItems = await db.collection('jungle').find().toArray(); 
+//   const cartCount = await db 
+//     .collection('jungle') 
+//     .countDocuments(); 
+  response.render('index.ejs'); 
 });
 
-app.post('/addItem', (request, response) => {
-  db.collection('jungle')
-    .insertOne({ thing: request.body.boughtItem }) 
-    .then((result) => {
-      console.log('Item Added'); 
-      response.redirect('/'); 
-    })
-    .catch((error) => console.error(error));
-});
+// app.post('/addItem', (request, response) => {
+//   db.collection('jungle')
+//     .insertOne({ thing: request.body.boughtItem }) 
+//     .then((result) => {
+//       console.log('Item Added'); 
+//       response.redirect('/'); 
+//     })
+//     .catch((error) => console.error(error));
+// });
 
-app.put('/changeQty', (request, response) => {
-  db.collection('jungle') 
-    .updateOne(
-      { thing: request.body.itemFromJS },
-      {
-        $set: {
-          quantity: 1,
-        },
-      },
-      {
-        upsert: false
-      }
-    )
-    .then((result) => {
-      console.log('Changed qty'); 
-      response.json('Changed qty'); 
-    })
-    .catch((error) => console.error(error)); 
-});
+// app.put('/changeQty', (request, response) => {
+//   db.collection('jungle') 
+//     .updateOne(
+//       { thing: request.body.itemFromJS },
+//       {
+//         $set: {
+//           quantity: 1,
+//         },
+//       },
+//       {
+//         upsert: false
+//       }
+//     )
+//     .then((result) => {
+//       console.log('Changed qty'); 
+//       response.json('Changed qty'); 
+//     })
+//     .catch((error) => console.error(error)); 
+// });
 
 
 
-app.delete('/deleteItem', (request, response) => {
-  db.collection('jungle')
-    .deleteOne({ thing: request.body.itemFromJS }) 
-    .then((result) => {
-      console.log('Item Deleted'); 
-      response.json('Item Deleted'); 
-    })
-    .catch((error) => console.error(error)); 
-});
+// app.delete('/deleteItem', (request, response) => {
+//   db.collection('jungle')
+//     .deleteOne({ thing: request.body.itemFromJS }) 
+//     .then((result) => {
+//       console.log('Item Deleted'); 
+//       response.json('Item Deleted'); 
+//     })
+//     .catch((error) => console.error(error)); 
+// });
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on port ${PORT}`);
