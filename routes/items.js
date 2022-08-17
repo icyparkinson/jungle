bc602a894a99f38eec7e8651699dd0a6aed53d85
koulight <koulight@hotmail.com>
2022-08-16 17:31:05 -0700

    Saving before attempting to create routes

--- routes/items.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const itemsController = require('../controllers/items') 
-// const { ensureAuth, ensureGuest } = require('../middleware/auth')
-
-router.get('/', itemsController.getItem)
-
-module.exports = router
